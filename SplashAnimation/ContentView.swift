929a221cb75806db6df965124ed921a0984597c4
Andrei Kamarou <mr.akomarov@gmail.com>
2021-10-18 23:17:35 +0300

    Initial Commit

---
+++ SplashAnimation/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  SplashAnimation
+//
+//  Created by Andrei Kamarou on 18.10.21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SplashAnimation/SplashAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SplashAnimationApp.swift
+//  SplashAnimation
+//
+//  Created by Andrei Kamarou on 18.10.21.
+//
+
+import SwiftUI
+
+@main
+struct SplashAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
